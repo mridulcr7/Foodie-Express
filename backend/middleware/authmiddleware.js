25f0e5a0e7eb0ff4099067b626d4dbe76a830a00
mridulcr7 <mridulpandey5277@gmail.com>
2023-12-21 04:05:45 +0530

    "now"

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require("../models/User");
-const config = process.env;
+require("dotenv").config();
 
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
   //  console.log(req.headers.authorization);
     if (token) {
-        jwt.verify(token, config.JWT_SECRET, async (err, decodedToken) => {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
             if (err) {
                 res.status(401).json({
                     success: false,
