228d6145fdd18f70f3cab3a7be7cf4ebb07d0335
mridulcr7 <mridulpandey5277@gmail.com>
2023-12-21 22:51:06 +0530

    "new"

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -12,7 +12,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('http://localhost:4000/signup', {
+        const response = await fetch('https://foodie-express.onrender/signup', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ name, email, password })
