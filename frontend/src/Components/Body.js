58eb8d8913d75c7978d2cf3c40866309f14ce4b4
mridulcr7 <mridulpandey5277@gmail.com>
2023-12-22 01:24:56 +0530

    "api fixed"

--- frontend/src/Components/Body.js
+++ frontend/src/Components/Body.js
@@ -14,7 +14,7 @@ const Body = () => {
     }, []);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.896551310069&lng=81.00563116371632&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6691565&lng=77.45375779999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
         setfilteredList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
