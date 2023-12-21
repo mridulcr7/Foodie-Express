6f6dc0af7e08ca99bbe206f843c09dc1ae5df3b7
mridulcr7 <mridulpandey5277@gmail.com>
2023-12-22 00:02:22 +0530

    "fixed cors"

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
