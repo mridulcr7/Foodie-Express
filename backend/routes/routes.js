2c2894d912af611206465aa9f0a919d2f296c42f
mridulcr7 <mridulpandey5277@gmail.com>
2023-12-21 20:59:50 +0530

    "deploy"

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
 const orderController = require('../controllers/orderController');
-const authMiddleware  = require('../middleware/authMiddleware');
+const authmiddleware  = require('../middleware/authmiddleware');
 
 const router = Router();
 
 router.post('/signup', authController.signup_post);
 router.post('/login', authController.login_post);
-router.post('/addorder', authMiddleware.requireAuth, orderController.addOrder);
-router.get('/getorders', authMiddleware.requireAuth, orderController.getOrders);
+router.post('/addorder', authmiddleware.requireAuth, orderController.addOrder);
+router.get('/getorders', authmiddleware.requireAuth, orderController.getOrders);
 
 
 module.exports = router;
